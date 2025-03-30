92fa50f5b0eed51504ea31de2fc486fdeb4dbaa4
Subham8988 <subham8988>
2025-03-30 11:25:58 +0530

    enquiry Form Design and fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'AMSF';
+
+  paymentReceiptUrl: string | null = null;
+
+  previewImage(event: any, previewId: string): void {
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.paymentReceiptUrl = reader.result as string; // Set the image URL to the variable
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  }
 }

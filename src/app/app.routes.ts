220ee3a86d6892accd56fe311e20fe511675f4d7
Subham8988 <subham8988>
2025-03-31 23:03:19 +0530

    eslint prettier added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { EnquiryFormComponent } from './enquiry-form/enquiry-form.component';
 
 export const routes: Routes = [
-    {
-        path:'enquiryForm',
-        component:EnquiryFormComponent
-    }
+  {
+    path: 'enquiryForm',
+    component: EnquiryFormComponent,
+  },
 ];
